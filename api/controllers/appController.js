1a4204dfe9d227489106d50bf528b66e1dc69835
Windana Rahubedde <Windana_105199@dialog.dialoggsm.com>
2019-07-31 15:41:23 +0530

    logging added

--- api/controllers/appController.js
+++ api/controllers/appController.js
@@ -1,11 +1,20 @@
 'use strict';
+const log = require('simple-node-logger').createSimpleLogger('project.log');
 
 exports.mo = (req, res) => {
+
+    log.info('MO request received. | at: ', new Date().toJSON());
+
     const { method, url, headers, body } = req;
-    console.log('MO request received.');
-    console.log(method);
-    console.log(url);
-    console.log(headers);
-    console.log(body);
+    log.info('method | ', method, ' | at: ', new Date().toJSON());
+    log.info('url | ', url, ' | at: ', new Date().toJSON());
+    log.info('headers | ', headers, ' | at: ', new Date().toJSON());
+    log.info('body | ', body, ' | at: ', new Date().toJSON());
+    
+    if(method == 'POST') {
+        var ussdAction = body.inboundUSSDMessageRequest.ussdAction;
+        log.info('ussdAction | ', ussdAction, ' | at: ', new Date().toJSON());
+    }
+
     res.send('OK');
 };
